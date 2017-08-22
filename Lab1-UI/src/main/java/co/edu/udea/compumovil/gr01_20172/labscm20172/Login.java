9ef258aa5d4f9b681791d2e630830cc9f6a40c78
JohnathanMB <johnathan.monsalve@udea.edu.co>
2017-08-21 19:46:08 -0500

    Initial commit

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20172/labscm20172/Login.java
@@ -0,0 +1,18 @@
+package co.edu.udea.compumovil.gr01_20172.labscm20172;
+
+import android.net.Uri;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gun0912.tedbottompicker.TedBottomPicker;
+
+public class Login extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
